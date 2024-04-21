39a3b28b4bba58db228470b6ebceff550a67a666
NicoletaFartan02 <nicoleta.fartan02@e-uvt.ro>
2024-04-21 22:34:59 +0300

    made the change

--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -3,15 +3,19 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-
 return function ($app) {
 
     // Setup route using the RouteService
     $app->get('/', '\App\Services\RouteService:helloWorld');
 
-    // Setup route for searching recipes by category
-    $app->get('/recipes/byCategory/{categoryId}', '\App\Services\RecipeHandler:getRecipesByCategoryId');
+    // Group for recipes routes
+    $app->group('/recipes', function () use ($app) {
+        // Setup route for searching recipes by category
+        $app->get('/byCategory/{categoryId}', '\App\Services\RecipeHandler:getRecipesByCategoryId');
 
-    $app->get('/recipes/{recipeId}', '\App\Services\RecipeHandlerId:getRecipeById');
+        // Setup route for getting recipe by ID
+        $app->get('/{recipeId}', '\App\Services\RecipeHandlerId:getRecipeById');
+    });
+
     return $app;
 };
